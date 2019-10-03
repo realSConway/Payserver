c6e5b4884ef5df9c2de90f7dda6a7793c6ff4833
SamuelC <samcon@protonmail.com>
2019-10-03 09:57:29 +0200

    upload files

---
+++ payment.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+		<head>
+				<style>
+						body {
+								background-color: MediumSeaGreen;
+
+}
+						h1 {
+								text-align: center;
+								font-family: Arial;
+
+}
+				</style>
+		</head>
+		<body>
+				<main>
+						<h1>
+bc1qv4klgumgvwvktflquwjunmxr42wshtd747rc27
+</h1></main></body></style></head></html>
