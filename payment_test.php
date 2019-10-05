3c2a82be2ba79f2edf1faee4faf5349bdd524d90
Samuel <samcon@protonmail.com>
2019-10-05 12:39:12 +0200

    Updated payment_test with css and added new div

--- payment_test.php
+++ payment_test.php
@@ -1,5 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Address page</title>
+		<meta name="robots">
+		<!-- Include CSS File Here-->
+		<link href="design_submit_button.css" rel="stylesheet">
+	</head>
+	<body>
+		<div id="main">
+			<div id="sub">
 <?php
-if (isset($_POST['submit'])) {
-		echo "bc1qv4klgumgvwvktflquwjunmxr42wshtd747rc27";
+if (isset($_POST)) {
+	echo "bc1qv4klgumgvwvktflquwjunmxr42wshtd747rc27";
 }
 ?>
+		</div>
+		</div>
+	</body>
+</html>
