f940fd02c8bdb6e19220674ae701b2032d3c01e0
Samuel <samcon@protonmail.com>
2019-10-17 23:00:51 +0200

    adjusted test payment page

--- payment_test.php
+++ payment_test.php
@@ -4,15 +4,13 @@
 		<title>Address page</title>
 		<meta name="robots">
 		<!-- Include CSS File Here-->
-		<link href="design_submit_button.css" rel="stylesheet">
+		<link href="./../design_submit_button.css" rel="stylesheet">
 	</head>
 	<body>
-		<div id="main">
 <?php
 if (isset($_POST)) {
 	echo "<div id=\"sub\"><p>bc1_TEST_payment_page_uwjunmxr42wshtd747rc27</p></div>";
 }
 ?>
-		</div>
 	</body>
 </html>
